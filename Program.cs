c4a51f3a66526823c28c193154d79dcd27cd1074
Kate <KhovanskayaK@yandex.ru>
2023-01-17 22:24:44 +0300

    добавлена задача 27

--- Program.cs
+++ Program.cs
@@ -1,17 +1,31 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-Console.WriteLine("Введите натуральное число А: ");
-int a=int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите натуральное число B: ");
-int b=int.Parse(Console.ReadLine()!);
-Console.WriteLine($"Число {a} в степени {b}: {Stepen(a,b)}");
+// Console.WriteLine("Введите натуральное число А: ");
+// int a=int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите натуральное число B: ");
+// int b=int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Число {a} в степени {b}: {Stepen(a,b)}");
 
-//Возведение в натуральную степень
-int Stepen(int a,int b){
-    int rez=1;
-    if(b>0){
-    for(int i=1; i<=b; i++){
-    rez=rez*a;
+// //Возведение в натуральную степень
+// int Stepen(int a,int b){
+//     int rez=1;
+//     if(b>0){
+//     for(int i=1; i<=b; i++){
+//     rez=rez*a;
+//     }
+//     }
+//     return  rez;
+// }
+
+//Задача 27:Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int num27=int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр числа {num27}: {SumNumbers(num27)}");
+//Сумма цифр в числе
+int SumNumbers(int num){
+    int sum=0;
+    while(num>0){
+        sum+=num%10;
+        num=num/10;
     }
-    }
-    return  rez;
+    return sum;
 }
